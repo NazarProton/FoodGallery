50f0e96dbb8ac119a7725e6b52ee94d99cc3a9dd
NP <nproton2012@gmail.com>
2021-05-18 01:02:51 +0300

    NP

---
+++ gallery-items.js
@@ -0,0 +1,172 @@
+export default [
+  {
+    description: "pizza1",
+    preview:
+    "https://images.pexels.com/photos/845802/pexels-photo-845802.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+    "https://images.pexels.com/photos/845802/pexels-photo-845802.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "cola, pizza2",
+    preview:
+      "https://images.pexels.com/photos/4109084/pexels-photo-4109084.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+    "https://images.pexels.com/photos/4109084/pexels-photo-4109084.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+     
+  },
+  {
+    description: "pizza3",
+    preview:
+      "https://images.pexels.com/photos/1552635/pexels-photo-1552635.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/1552635/pexels-photo-1552635.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "soup",
+    preview:
+      "https://images.pexels.com/photos/1618921/pexels-photo-1618921.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/1618921/pexels-photo-1618921.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "lunch",
+    preview:
+      "https://images.pexels.com/photos/3186654/pexels-photo-3186654.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/3186654/pexels-photo-3186654.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "burger1",
+    preview:
+      "https://images.pexels.com/photos/1199960/pexels-photo-1199960.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/1199960/pexels-photo-1199960.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "burger2",
+    preview:
+      "https://images.pexels.com/photos/2983101/pexels-photo-2983101.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/2983101/pexels-photo-2983101.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "boul",
+    preview:
+      "https://images.pexels.com/photos/1860208/pexels-photo-1860208.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/1860208/pexels-photo-1860208.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "pizza4",
+    preview:
+      "https://images.pexels.com/photos/905847/pexels-photo-905847.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/905847/pexels-photo-905847.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "sushi",
+    preview:
+      "https://images.pexels.com/photos/2323398/pexels-photo-2323398.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/2323398/pexels-photo-2323398.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "sushi2",
+    preview:
+      "https://images.pexels.com/photos/359993/pexels-photo-359993.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/359993/pexels-photo-359993.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "sushi3",
+    preview:
+      "https://images.pexels.com/photos/858501/pexels-photo-858501.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/858501/pexels-photo-858501.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "steak",
+    preview:
+      "https://images.pexels.com/photos/7866189/pexels-photo-7866189.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/7866189/pexels-photo-7866189.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "chicken",
+    preview:
+    "https://images.pexels.com/photos/5175618/pexels-photo-5175618.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+    "https://images.pexels.com/photos/5175618/pexels-photo-5175618.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "tacos",
+    preview:
+      "https://images.pexels.com/photos/7388096/pexels-photo-7388096.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/7388096/pexels-photo-7388096.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "kebab",
+    preview:
+    "https://images.pexels.com/photos/5175632/pexels-photo-5175632.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+    "https://images.pexels.com/photos/5175632/pexels-photo-5175632.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "soup2",
+    preview:
+      "https://images.pexels.com/photos/5605529/pexels-photo-5605529.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/5605529/pexels-photo-5605529.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "steak1",
+    preview:
+      "https://images.pexels.com/photos/7333164/pexels-photo-7333164.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/7333164/pexels-photo-7333164.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "steak2",
+    preview:
+      "https://images.pexels.com/photos/361184/asparagus-steak-veal-steak-veal-361184.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/361184/asparagus-steak-veal-steak-veal-361184.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "steak3",
+    preview:
+      "https://images.pexels.com/photos/4101805/pexels-photo-4101805.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/4101805/pexels-photo-4101805.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "khinkali",
+    preview:
+      "https://images.pexels.com/photos/5898838/pexels-photo-5898838.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/5898838/pexels-photo-5898838.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "soup5",
+    preview:
+      "https://images.pexels.com/photos/3026808/pexels-photo-3026808.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/3026808/pexels-photo-3026808.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "seafood",
+    preview:
+      "https://images.pexels.com/photos/628776/pexels-photo-628776.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/628776/pexels-photo-628776.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    description: "breakfast",
+    preview:
+      "https://images.pexels.com/photos/3850838/pexels-photo-3850838.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    original:
+      "https://images.pexels.com/photos/3850838/pexels-photo-3850838.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+
+];
---
+++ scripts.js
@@ -0,0 +1,113 @@
+import imagesRefs from "./gallery-items.js";
+
+const refs = {
+    gallery: document.querySelector(".js-gallery"),
+    modalRef: document.querySelector(".lightbox"),
+    imageRef: document.querySelector(".lightbox__image"),
+    prev: document.querySelector(".btn-prev"),
+    next: document.querySelector(".btn-next"),
+    overlayRef: document.querySelector(".lightbox__overlay"),
+    btn: document.querySelector(".lightbox__button"),
+    ulRef: document.querySelector("ul"),
+  };
+  
+  class Gallery {
+    constructor(elements, elementsRefs) {
+      this.imagesRefs = elements;
+      this.refs = elementsRefs;
+    }
+  
+    newImagesOnPage() {
+      imagesRefs.forEach(({ description, preview, original }) => {
+        refs.ulRef.innerHTML += `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${original}" data-source="${preview}" alt="${description}"/>
+        </a>
+        </li>`;
+      });
+    }
+  
+    addListener() {
+      refs.gallery.addEventListener("click", ongalleryClick);
+  
+      function ongalleryClick(event) {
+        event.preventDefault();
+  
+        let slides = document.querySelectorAll(".gallery__image");
+        let arr = Array.from(slides);
+  
+        if (event.target.nodeName !== "IMG") {
+          return;
+        }
+  
+        refs.modalRef.classList.add("is-open");
+        refs.imageRef.src = event.target.src;
+  
+        {
+          let slideIndex = arr.indexOf(event.target);
+  
+          const activeSlide = (n) => {
+            refs.imageRef.src = slides[n].src;
+          };
+  
+          const nextSlide = () => {
+            if (slideIndex == slides.length - 1) {
+              slideIndex = 0;
+              activeSlide(slideIndex);
+            } else {
+              slideIndex++;
+              activeSlide(slideIndex);
+            }
+          };
+  
+          const prevSlide = () => {
+            if (slideIndex == 0) {
+              slideIndex = slides.length - 1;
+              activeSlide(slideIndex);
+            } else {
+              slideIndex--;
+              activeSlide(slideIndex);
+            }
+          };
+  
+          refs.next.addEventListener("click", nextSlide);
+          refs.prev.addEventListener("click", prevSlide);
+  
+          window.addEventListener("keydown", (event) => {
+            if (event.code === "ArrowRight") {
+              nextSlide();
+            }
+          });
+  
+          window.addEventListener("keydown", (event) => {
+            if (event.code === "ArrowLeft") {
+              prevSlide();
+            }
+          });
+        }
+  
+        window.addEventListener("keydown", (event) => {
+          if (event.key === "Escape") {
+            refs.modalRef.classList.remove("is-open");
+            document.body.style.overflow = "";
+          }
+        });
+  
+        refs.overlayRef.addEventListener("click", () => {
+          refs.modalRef.classList.remove("is-open");
+          document.body.style.overflow = "";
+        });
+  
+        refs.btn.addEventListener("click", () => {
+          refs.modalRef.classList.remove("is-open");
+          document.body.style.overflow = "";
+        });
+        document.body.style.overflow = "hidden";
+      }
+    }
+  }
+  
+  const gallery = new Gallery(imagesRefs, refs);
+  
+  gallery.newImagesOnPage();
+  gallery.addListener();
